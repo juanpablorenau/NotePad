5a983dc0eca903e4c12a10cb626211abd609d160
jrenau <jrenau@mercadona.es>
2024-09-23 16:53:04 +0200

    feat/note_detail: Add elements

--- app/src/main/java/com/example/notepad/ui/detail/NoteDetailBottomBar.kt
+++ app/src/main/java/com/example/notepad/ui/detail/NoteDetailBottomBar.kt
@@ -6,9 +6,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,7 +29,8 @@ fun NoteDetailBottomBar(
             .fillMaxWidth()
             .background(MaterialTheme.colorScheme.tertiary)
             .padding(vertical = 8.dp),
-        horizontalArrangement = Arrangement.SpaceEvenly
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(
             painter = painterResource(id = R.drawable.ic_text_format),
@@ -36,16 +39,23 @@ fun NoteDetailBottomBar(
         )
 
         Icon(
-            modifier = Modifier.clickable { addCheckBox(null) },
-            painter = painterResource(id = R.drawable.ic_check_box),
+            modifier = Modifier.clickable { addTextField() },
+            painter = painterResource(id = R.drawable.ic_text_fields),
+            contentDescription = "text fields icon",
+            tint = MaterialTheme.colorScheme.primary
+        )
+
+        Icon(
+            modifier = Modifier.size(32.dp).clickable { addCheckBox(null) },
+            painter = painterResource(id = R.drawable.ic_check_list),
             contentDescription = "check box icon",
             tint = MaterialTheme.colorScheme.primary
         )
 
         Icon(
-            modifier = Modifier.clickable { addTextField() },
-            painter = painterResource(id = R.drawable.ic_text_fields),
-            contentDescription = "text fields icon",
+            modifier = Modifier.size(24.dp),
+            painter = painterResource(id = R.drawable.ic_grid),
+            contentDescription = "grid icon",
             tint = MaterialTheme.colorScheme.primary
         )
 
