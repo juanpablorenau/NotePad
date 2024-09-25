fbaf0ca0c664ba625a634ac5b15335a5b12afc68
jrenau <jrenau@mercadona.es>
2024-09-25 13:57:54 +0200

    feat/note_detail: Finish text format

--- app/src/main/java/com/example/notepad/ui/detail/NoteDetailBottomBar.kt
+++ app/src/main/java/com/example/notepad/ui/detail/NoteDetailBottomBar.kt
@@ -1,5 +1,6 @@
 package com.example.notepad.ui.detail
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -11,16 +12,16 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -61,7 +62,7 @@ fun BottomOptions(
                 showBottomSheet.value = true
                 keyboardController?.hide()
             },
-            painter = painterResource(id = R.drawable.ic_text_format),
+            painter = painterResource(id = R.drawable.ic_border_color),
             contentDescription = "text format icon",
             tint = MaterialTheme.colorScheme.primary
         )
@@ -106,14 +107,18 @@ fun TextFormatComponent(
 
     Card(
         modifier = Modifier.fillMaxWidth(),
+        border = BorderStroke(
+            width = 1.dp,
+            color = MaterialTheme.colorScheme.tertiary
+        ),
         shape = RoundedCornerShape(
             topStart = 16.dp, topEnd = 16.dp, bottomStart = 0.dp, bottomEnd = 0.dp
         ),
-        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 12.dp),
     ) {
         Column(
             modifier = Modifier
-                .background(MaterialTheme.colorScheme.tertiary)
+                .background(MaterialTheme.colorScheme.background)
                 .padding(12.dp),
         ) {
             Row(
@@ -135,21 +140,37 @@ fun TextFormatComponent(
                     },
                     painter = painterResource(id = R.drawable.ic_close),
                     contentDescription = "close icon",
-                    tint = MaterialTheme.colorScheme.primary
+                    tint = MaterialTheme.colorScheme.onBackground
                 )
+
+                TextFormatContent()
             }
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            TypeTextsSelector()
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
+fun TextFormatContent(){
+    Spacer(modifier = Modifier.height(24.dp))
+
+    TypeTextsSelector()
+
+    Spacer(modifier = Modifier.height(18.dp))
+
+    FormatTextsSelector()
+
+    Spacer(modifier = Modifier.height(12.dp))
+
+    ParagraphsSelectorAndTextColor()
+
+    Spacer(modifier = Modifier.height(24.dp))
+}
+
+@Preview(showBackground = true)
+@Composable
 fun TypeTextsSelector() {
-    val selectedIndex = remember { mutableStateOf(-1) }
+    val selectedIndex = remember { mutableIntStateOf(-1) }
 
     val typeTexts = remember {
         listOf(
@@ -177,25 +198,356 @@ fun TypeTextsSelector() {
 fun TypeTextsItem(
     index: Int = -1,
     typeText: TypeText = TypeText("Title", 24, true),
-    selectedIndex: MutableState<Int> = mutableStateOf(-1),
+    selectedIndex: MutableState<Int> = mutableIntStateOf(-1),
 ) {
     Card(
-        modifier = Modifier.clickable { selectedIndex.value = index },
+        modifier = Modifier
+            .height(40.dp)
+            .clickable { selectedIndex.value = index },
         shape = RoundedCornerShape(12.dp),
         colors = CardDefaults.cardColors(
             containerColor =
             if (selectedIndex.value == index) MaterialTheme.colorScheme.primary
-            else MaterialTheme.colorScheme.tertiary
+            else MaterialTheme.colorScheme.background
         ),
     ) {
-        Text(
-            modifier = Modifier.padding(vertical = 4.dp, horizontal = 16.dp),
-            text = typeText.text,
-            fontSize = typeText.fontSize.sp,
-            fontWeight = if (typeText.isBold) FontWeight.Bold else FontWeight.Normal,
-            color =
-            if (selectedIndex.value == index) MaterialTheme.colorScheme.background
-            else MaterialTheme.colorScheme.onBackground,
-        )
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(
+                modifier = Modifier.padding(vertical = 4.dp, horizontal = 16.dp),
+                text = typeText.text,
+                fontSize = typeText.fontSize.sp,
+                fontWeight = if (typeText.isBold) FontWeight.Bold else FontWeight.Normal,
+                color =
+                if (selectedIndex.value == index) MaterialTheme.colorScheme.background
+                else MaterialTheme.colorScheme.secondary,
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FormatTextsSelector() {
+    val selectedIndexes = remember { mutableStateListOf(false, false, false, false) }
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        Card(
+            modifier = Modifier
+                .height(32.dp)
+                .weight(1f)
+                .clickable { selectedIndexes[0] = !selectedIndexes[0] },
+            shape = RoundedCornerShape(
+                topStart = 12.dp,
+                topEnd = 0.dp,
+                bottomStart = 12.dp,
+                bottomEnd = 0.dp
+            ),
+            colors = CardDefaults.cardColors(
+                containerColor =
+                if (selectedIndexes[0]) MaterialTheme.colorScheme.primary
+                else MaterialTheme.colorScheme.tertiary
+            ),
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    modifier = Modifier.padding(vertical = 4.dp, horizontal = 16.dp),
+                    text = "B",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    color =
+                    if (selectedIndexes[0]) MaterialTheme.colorScheme.background
+                    else MaterialTheme.colorScheme.secondary,
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.width(1.dp))
+
+        Card(
+            modifier = Modifier
+                .height(32.dp)
+                .weight(1f)
+                .clickable { selectedIndexes[1] = !selectedIndexes[1] },
+            shape = RoundedCornerShape(
+                topStart = 0.dp,
+                topEnd = 0.dp,
+                bottomStart = 0.dp,
+                bottomEnd = 0.dp
+            ),
+            colors = CardDefaults.cardColors(
+                containerColor =
+                if (selectedIndexes[1]) MaterialTheme.colorScheme.primary
+                else MaterialTheme.colorScheme.tertiary
+            ),
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    modifier = Modifier.padding(vertical = 4.dp, horizontal = 16.dp),
+                    text = "I",
+                    fontSize = 16.sp,
+                    fontStyle = FontStyle.Italic,
+                    color =
+                    if (selectedIndexes[1]) MaterialTheme.colorScheme.background
+                    else MaterialTheme.colorScheme.secondary,
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.width(1.dp))
+
+        Card(
+            modifier = Modifier
+                .height(32.dp)
+                .weight(1f)
+                .clickable { selectedIndexes[2] = !selectedIndexes[2] },
+            shape = RoundedCornerShape(
+                topStart = 0.dp,
+                topEnd = 0.dp,
+                bottomStart = 0.dp,
+                bottomEnd = 0.dp
+            ),
+            colors = CardDefaults.cardColors(
+                containerColor =
+                if (selectedIndexes[2]) MaterialTheme.colorScheme.primary
+                else MaterialTheme.colorScheme.tertiary
+            ),
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    modifier = Modifier.padding(vertical = 4.dp, horizontal = 16.dp),
+                    text = "U",
+                    fontSize = 16.sp,
+                    style = TextStyle(textDecoration = TextDecoration.Underline),
+                    color =
+                    if (selectedIndexes[2]) MaterialTheme.colorScheme.background
+                    else MaterialTheme.colorScheme.secondary,
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.width(1.dp))
+
+        Card(
+            modifier = Modifier
+                .height(32.dp)
+                .weight(1f)
+                .clickable { selectedIndexes[3] = !selectedIndexes[3] },
+            shape = RoundedCornerShape(
+                topStart = 0.dp,
+                topEnd = 12.dp,
+                bottomStart = 0.dp,
+                bottomEnd = 12.dp
+            ),
+            colors = CardDefaults.cardColors(
+                containerColor =
+                if (selectedIndexes[3]) MaterialTheme.colorScheme.primary
+                else MaterialTheme.colorScheme.tertiary
+            ),
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    modifier = Modifier.padding(vertical = 4.dp, horizontal = 16.dp),
+                    text = "S",
+                    fontSize = 16.sp,
+                    style = TextStyle(textDecoration = TextDecoration.LineThrough),
+                    color =
+                    if (selectedIndexes[3]) MaterialTheme.colorScheme.background
+                    else MaterialTheme.colorScheme.secondary,
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ParagraphsSelectorAndTextColor() {
+    val selectedIndex = remember { mutableIntStateOf(-1) }
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+
+        Card(
+            modifier = Modifier
+                .height(32.dp)
+                .weight(1f),
+            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.tertiary),
+            shape = RoundedCornerShape(
+                topStart = 12.dp,
+                topEnd = 12.dp,
+                bottomStart = 12.dp,
+                bottomEnd = 12.dp
+            ),
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    modifier = Modifier.size(16.dp),
+                    painter = painterResource(id = R.drawable.ic_text_format),
+                    contentDescription = "text color icon",
+                    tint = MaterialTheme.colorScheme.secondary,
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.width(12.dp))
+
+        Row(
+            modifier = Modifier.weight(4f)
+        ) {
+            Card(
+                modifier = Modifier
+                    .height(32.dp)
+                    .weight(1f)
+                    .clickable { selectedIndex.intValue = 0 },
+                shape = RoundedCornerShape(
+                    topStart = 12.dp,
+                    topEnd = 0.dp,
+                    bottomStart = 12.dp,
+                    bottomEnd = 0.dp
+                ),
+                colors = CardDefaults.cardColors(
+                    containerColor =
+                    if (selectedIndex.intValue == 0) MaterialTheme.colorScheme.primary
+                    else MaterialTheme.colorScheme.tertiary
+                ),
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        modifier = Modifier.size(16.dp),
+                        painter = painterResource(id = R.drawable.ic_format_align_left),
+                        contentDescription = "text color icon",
+                        tint =
+                        if (selectedIndex.intValue == 0) MaterialTheme.colorScheme.background
+                        else MaterialTheme.colorScheme.secondary,
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.width(1.dp))
+
+            Card(
+                modifier = Modifier
+                    .height(32.dp)
+                    .weight(1f)
+                    .clickable { selectedIndex.intValue = 1 },
+                shape = RoundedCornerShape(
+                    topStart = 0.dp,
+                    topEnd = 0.dp,
+                    bottomStart = 0.dp,
+                    bottomEnd = 0.dp
+                ),
+                colors = CardDefaults.cardColors(
+                    containerColor =
+                    if (selectedIndex.intValue == 1) MaterialTheme.colorScheme.primary
+                    else MaterialTheme.colorScheme.tertiary
+                ),
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        modifier = Modifier.size(16.dp),
+                        painter = painterResource(id = R.drawable.ic_format_align_justify),
+                        contentDescription = "text color icon",
+                        tint =
+                        if (selectedIndex.intValue == 1) MaterialTheme.colorScheme.background
+                        else MaterialTheme.colorScheme.secondary,
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.width(1.dp))
+
+            Card(
+                modifier = Modifier
+                    .height(32.dp)
+                    .weight(1f)
+                    .clickable { selectedIndex.intValue = 2 },
+                shape = RoundedCornerShape(
+                    topStart = 0.dp,
+                    topEnd = 0.dp,
+                    bottomStart = 0.dp,
+                    bottomEnd = 0.dp
+                ),
+                colors = CardDefaults.cardColors(
+                    containerColor =
+                    if (selectedIndex.intValue == 2) MaterialTheme.colorScheme.primary
+                    else MaterialTheme.colorScheme.tertiary
+                ),
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        modifier = Modifier.size(16.dp),
+                        painter = painterResource(id = R.drawable.ic_format_align_center),
+                        contentDescription = "text color icon",
+                        tint =
+                        if (selectedIndex.intValue == 2) MaterialTheme.colorScheme.background
+                        else MaterialTheme.colorScheme.secondary,
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.width(1.dp))
+
+            Card(
+                modifier = Modifier
+                    .height(32.dp)
+                    .weight(1f)
+                    .clickable { selectedIndex.intValue = 3 },
+                shape = RoundedCornerShape(
+                    topStart = 0.dp,
+                    topEnd = 12.dp,
+                    bottomStart = 0.dp,
+                    bottomEnd = 12.dp
+                ),
+                colors = CardDefaults.cardColors(
+                    containerColor =
+                    if (selectedIndex.intValue == 3) MaterialTheme.colorScheme.primary
+                    else MaterialTheme.colorScheme.tertiary
+                ),
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        modifier = Modifier.size(16.dp),
+                        painter = painterResource(id = R.drawable.ic_format_align_right),
+                        contentDescription = "text color icon",
+                        tint =
+                        if (selectedIndex.intValue == 3) MaterialTheme.colorScheme.background
+                        else MaterialTheme.colorScheme.secondary,
+                    )
+                }
+            }
+        }
     }
 }
--- model/src/main/java/com/example/model/entities/TypeText.kt
+++ model/src/main/java/com/example/model/entities/TypeText.kt
@@ -4,4 +4,6 @@ data class TypeText(
     val text: String = "",
     val fontSize : Int = 24,
     val isBold : Boolean = false,
+    val isItalic : Boolean = false,
+    val isUnderline : Boolean = false
 )
