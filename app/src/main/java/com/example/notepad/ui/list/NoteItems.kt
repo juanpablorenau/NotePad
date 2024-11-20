c9a545c154c0cf99980edf7fb62a8f9dbf1f5797
jrenau <jrenau@mercadona.es>
2024-11-20 16:56:28 +0100

    feat/add_flows: Version 1.13
    - Fix Annonated String fluency

--- app/src/main/java/com/example/notepad/ui/list/NoteItems.kt
+++ app/src/main/java/com/example/notepad/ui/list/NoteItems.kt
@@ -17,16 +17,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,11 +47,7 @@ fun TextItem(
     isDarkTheme: Boolean = false,
     maxLines: Int = 1,
 ) {
-    var annotatedString by remember { mutableStateOf(AnnotatedString(noteItem.text)) }
-
-    LaunchedEffect(noteItem, isDarkTheme) {
-        annotatedString = getAnnotatedString(noteItem, isDarkTheme)
-    }
+    val annotatedString by remember { mutableStateOf(getAnnotatedString(noteItem, isDarkTheme)) }
 
     Text(
         modifier = Modifier.fillMaxWidth(),
